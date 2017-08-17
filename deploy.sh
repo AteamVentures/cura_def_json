b7726f08d5fb763bbacfb126b65ca296b490395e
Joseph Choi <chousemath@gmail.com>
2017-08-17 11:19:47 +0900

    trying to deploy again

--- deploy.sh
+++ deploy.sh
@@ -11,12 +11,6 @@ read comment
 echo -e "${GREEN}commit comment has been loaded${NC}"
 # just a spacer
 echo -e "${LIGHTCYAN}............${NC}"
-
-echo -e "${GREEN}npm run deploy started${NC}"
-npm run deploy
-echo -e "${GREEN}npm run deploy completed${NC}"
-echo -e "${LIGHTCYAN}............${NC}"
-
 echo -e "${GREEN}git add . started${NC}"
 git add .
 echo -e "${GREEN}git add . completed${NC}"
@@ -28,5 +22,9 @@ echo -e "${LIGHTCYAN}............${NC}"
 echo -e "${GREEN}git push started${NC}"
 git push
 echo -e "${GREEN}git push completed${NC}"
+echo -e "${LIGHTCYAN}............${NC}"
+echo -e "${GREEN}npm run deploy started${NC}"
+npm run deploy
+echo -e "${GREEN}npm run deploy completed${NC}"
 echo -e "${LIGHTCYAN}............${NC}"
 echo -e "${GREEN}deployment complete!${NC}"
